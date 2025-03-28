3e3b1703ab2dbbd91579e1694f5f729726806b14
kimboxu <kimboxu@gmail.com>
2025-03-28 19:17:38 +0900

    add chat debuging message

--- make_remake/Chzzk_chat_message.py
+++ make_remake/Chzzk_chat_message.py
@@ -136,8 +136,7 @@ class chzzk_chat_message:
                 except asyncio.TimeoutError:
                     if init.chzzk_titleData.loc[chzzkChat.chzzkID, 'live_state'] == "OPEN":
                         chzzkChat.Join_count += 1
-                    asyncio.create_task(async_errorPost(f"Test.{chzzkChat.Join_count}.{chzzkChat.chzzkID}.{datetime.now()}"))
-                    # await asyncio.sleep(0.05)
+                    # asyncio.create_task(async_errorPost(f"Test.{chzzkChat.Join_count}.{chzzkChat.chzzkID}.{datetime.now()}"))
                     continue
                     
                 except (JSONDecodeError, ConnectionError, RuntimeError, websockets.exceptions.ConnectionClosed) as e:
