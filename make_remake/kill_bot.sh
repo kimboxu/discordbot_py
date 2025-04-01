86994dc6e3c5e7dfd3692f62d70b211f3ffdd3ad
kimboxu <kimboxu@gmail.com>
2025-04-01 13:01:33 +0900

    del kill bot

--- make_remake/kill_bot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pid=$(pgrep -f "python3 discordbot.py")
-if [ -n "$pid" ]; then
-    kill -9 $pid
-    echo "Discord bot killed (PID: $pid)"
-else
-    echo "Discord bot not running"
-fi
